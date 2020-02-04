bebb95e0a239d6976ac91eacb2e0baff8e6690ca
Heather Cook (student) <2375346C@student.gla.ac.uk>
2020-02-04 16:35:14 +0000

    almost done tests

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(Page):
+    list_display = ('title', 'category', 'URL')
+
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Page)
+
