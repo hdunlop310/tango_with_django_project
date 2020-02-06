aeeff27462599819f8a2518b36a74ca5076e453f
Heather Cook <2375346c@student.gla.ac.uk>
2020-02-06 10:39:32 +0000

    finished chap5 passses tests

--- populate_rango.py
+++ populate_rango.py
@@ -11,7 +11,7 @@ def populate():
     python_pages = [
         {'title':'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think Like a Computer Scientist',
+        {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'} ]
@@ -21,7 +21,7 @@ def populate():
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango With Django',
+        {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/'} ]
 
     other_pages = [
